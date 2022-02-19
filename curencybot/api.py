ac5f65c844f4142cfe7bd8f090e4d7c0d98c0fff
vladmin83 <vladikmin83@yandex.ru>
2022-02-20 01:05:56 +0300

    corr key

--- curencybot/api.py
+++ curencybot/api.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-base_key = 'USD'
-sym_key = 'RUB'
+base_key = 'USD_RUB'
+sym_key = 'RUB_USD'
 amount = 100
 
 
-r = requests.get(f"https://free.currconv.com/api/v7/convert?q=USD_RUB,RUB_USD&compact=ultra&apiKey=2f98ff25fe53cb6c6e61")
+r = requests.get(f"https://free.currconv.com/api/v7/convert?q={base_key},{sym_key}&compact=ultra&apiKey=2f98ff25fe53cb6c6e61")
 
 resp = json.loads(r.content)
 new_price = resp[sym_key] * amount
