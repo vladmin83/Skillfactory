69f1d1f004d673160463d2f2be51253d4a69e6e4
vladmin83 <vladikmin83@yandex.ru>
2022-02-22 20:59:24 +0300

    replace in congig

--- curencybot/config.py
+++ curencybot/config.py
@@ -1,3 +1,7 @@
+import requests
+import json
+
+
 exchanges = {
     'доллар': 'RUB_USD',
     #'евро': 'EUR',
@@ -5,3 +9,14 @@ exchanges = {
 }
 
 TOKEN = "5190770347:AAG2UzUtbOzqlWXc4aYoFUW72kk16tTnvmY"
+
+base_key = 'USD_RUB'
+sym_key = 'RUB_USD'
+amount = 100
+
+
+r = requests.get(f"https://free.currconv.com/api/v7/convert?q={base_key},{sym_key}&compact=ultra&apiKey=2f98ff25fe53cb6c6e61")
+
+resp = json.loads(r.content)
+new_price = resp[sym_key] * amount
+print(new_price)
